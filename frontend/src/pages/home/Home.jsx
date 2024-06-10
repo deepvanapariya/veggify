8516a8c994ad476c348df70fb4825cfc19de83f3
Deep <deep.71.v@gmail.com>
2024-06-10 08:53:21 +0530

    hero section added

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Hero from "../../component/Hero";
+import CategoryWrapper from "../category/CategoryWrapper";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container mx-auto ">
+      <div className="flex flex-col justify-center items-center w-full py-20">
+        <Hero />
+        <CategoryWrapper />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
